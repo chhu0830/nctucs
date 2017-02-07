9933cf91f674ee48f76ac20eaf78bcee0b752ebe
oHSUANo <chhu0830@gmail.com>
2017-02-07 17:58:45 +0800

    Add posts writer

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 	has_many :viewlists, :dependent=>:delete_all
 	has_many :boards, :through=>:viewlists
 	has_many :categories
+	has_many :posts, :dependent=>:destroy
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
